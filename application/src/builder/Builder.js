b218597925b71c9430e50ee5a886f59a67a1dfdd
Zoe Krucky <zkrucky93@gmail.com>
2021-06-25 23:21:16 +0000

    added dragdrop component

--- application/src/builder/Builder.js
+++ application/src/builder/Builder.js
@@ -2,22 +2,32 @@ import React, {Component} from 'react';
 
 import {Container, Row, Col} from "reactstrap";
 
+import{DragDropContext} from "react-beautiful-dnd";
+
 import Menu from "./Menu";
+import Workbench from "./Workbench";
 
 export default class Builder extends Component{
+    onDragEnd = result => {
+        //TODO
+    };
+
     render() {
         return(
             <div className="base">
                 <Container className="builder" fluid={true}>
-                    <Row>
-                        <Col className="instructions">
-                            <p className="section-title">INSTRUCTIONS</p>
-                            <Menu/>
-                        </Col>
-                        <Col className="workbench">
-                            <p className="section-title">WORKBENCH</p>
-                        </Col>
-                    </Row>
+                    <DragDropContext onDragEnd={this.onDragEnd}>
+                        <Row>
+                            <Col className="instructions">
+                                <p className="section-title">INSTRUCTIONS</p>
+                                <Menu/>
+                            </Col>
+                            <Col className="workbench">
+                                <p className="section-title">WORKBENCH</p>
+                                <Workbench/>
+                            </Col>
+                        </Row>
+                    </DragDropContext>
                     <Row>
                     <Col className="code">
                             <p className="section-title">CODE</p>
