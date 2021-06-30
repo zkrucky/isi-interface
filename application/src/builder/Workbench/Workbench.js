bc938aee7298cae18d1f7083ab4e34af5b176e55
Zoe Krucky <zkrucky93@gmail.com>
2021-06-30 01:35:47 +0000

    added warning modal for delete all button

--- application/src/builder/Workbench/Workbench.js
+++ application/src/builder/Workbench/Workbench.js
@@ -1,16 +1,42 @@
 import React, {Component} from 'react';
 
-import {Button} from 'reactstrap';
+import {Button, Modal, ModalBody, ModalFooter} from 'reactstrap';
 
 import WorkingBlocks from "./WorkingBlocks";
+import TrashIcon from "../../static/images/trash.svg";
 
 export default class Workbench extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            modalToggle: false
+        }
+
+        this.toggleModal = this.toggleModal.bind(this);
+    }
+
     render(){
         return(
             <>
-                <Button onClick={() => {this.props.clearBlocks()}}>Remove All Blocks</Button>
                 <WorkingBlocks workingBlocks={this.props.workingBlocks} removeWorkingBlock={this.props.removeWorkingBlock}/>
+                <div className="trash-button">
+                    <Button color="" onClick={this.toggleModal}><img src={TrashIcon}/></Button>
+                    <Modal isOpen={this.state.modalToggle} toggle={this.toggleModal}>
+                        <ModalBody>
+                            Are you sure you want to clear the workbench? All work will be erased.
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button color="" onClick={() => {this.toggleModal(); this.props.clearBlocks()}}>Delete All</Button>
+                            <Button color="" onClick={() => {this.toggleModal()}}>Cancel</Button>
+                        </ModalFooter>
+                    </Modal>
+                </div>
             </>
         );
+    }
+
+    toggleModal(){
+        this.setState({modalToggle: !this.state.modalToggle});
     }
 }
