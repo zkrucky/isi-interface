3e84e93f45678c21b59e3ae8447027b70658e0a1
Zoe Krucky <zkrucky93@gmail.com>
2021-07-20 02:19:00 +0000

    helper function added

--- application/src/builder/Workbench/LoadModal.js
+++ application/src/builder/Workbench/LoadModal.js
@@ -7,10 +7,14 @@ export default class LoadModal extends Component {
         super(props);
 
         this.state = {
-            modalToggle: false
+            modalToggle: false,
+            fileText: "",
+            fileType: ""
         }
 
         this.toggleModal = this.toggleModal.bind(this);
+        this.processFileImport = this.processFileImport.bind(this);
+        this.createBlocksFromImport = this.createBlocksFromImport.bind(this);
     }
 
     render(){
@@ -19,10 +23,10 @@ export default class LoadModal extends Component {
                 <Button color="" onClick={() => {this.toggleModal()}}><img src={LoadIcon}/></Button>
                 <Modal isOpen={this.state.modalToggle} toggle={this.toggleModal}>
                     <ModalBody>
-                        <Button>Upload file</Button>
+                        <Input type="file" accept="application/json" onChange={(event) => this.processFileImport(event.target.files[0])}/>
                     </ModalBody>
                     <ModalFooter>
-                        <Button color="">Load</Button>
+                        <Button color="" disabled={!this.state.fileText} onClick={this.createBlocksFromImport}>Load</Button>
                         <Button color="" onClick={() => {this.toggleModal()}}>Close</Button>
                     </ModalFooter>
                 </Modal>
@@ -32,5 +36,13 @@ export default class LoadModal extends Component {
 
     toggleModal(){
         this.setState({modalToggle: !this.state.modalToggle});
+    }
+
+    processFileImport(file){
+        const reader = new FileReader();
+        reader.onload = () => {
+            this.setState({fileType: file.type, fileText: reader.result});
+        }
+        reader.readAsText(file);
     }
 }
