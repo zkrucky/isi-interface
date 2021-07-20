7fcba9644c421aaeb17c91a8b564f53ebb25ceee
Zoe Krucky <zkrucky93@gmail.com>
2021-07-20 19:06:02 +0000

    removed console.log statements

--- application/src/builder/Workbench/LoadModal.js
+++ application/src/builder/Workbench/LoadModal.js
@@ -48,8 +48,6 @@ export default class LoadModal extends Component {
 
     createBlocksFromImport(){
         let blocks = JSON.parse(this.state.fileText);
-        console.log(blocks.blocks);
         this.props.setBlocks(blocks.blocks);
-        console.log(this.props.workingBlocks);
     }
 }
